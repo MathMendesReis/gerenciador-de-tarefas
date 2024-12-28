69555e322870394fe4383824c3d68996ef8e5f5d
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-28 08:35:07 -0300

    feat: create server with express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-import express from 'express'
-import {Request,Response} from 'express'
-const app = express()
+import { app } from './app'
 
-app.get('/', function (req:Request, res:Response) {
-  res.send('Hello World')
+app.listen(3000,()=>{
+  console.log('Serve is running')
 })
-
-app.listen(3000)
